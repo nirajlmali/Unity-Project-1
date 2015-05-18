174be525e5a60584f32ace294cf957e1e04cdb2d
unknown <malin383@CE105WD554110S.hillnet.ad.hsd.k12.or.us>
2015-05-18 15:31:17 -0700

    Changes

---
+++ Unity 2D Project/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour 
+{
+	public Animator anim;
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+
+	}
+	void Update()
+	{
+		float move = Input.GetAxis ("Vertical");
+		anim.SetFloat ("Speed", move);
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			Vector2 position = this.transform.position;
+			position.y++;
+			this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			Vector2 position = this.transform.position;
+			position.y--;
+			this.transform.position = position;
+		}
+	}
+
+
+}
