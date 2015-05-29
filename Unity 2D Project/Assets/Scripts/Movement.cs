35e0bd6f0f7b48126e74ed26c20cf7e45b0b2ebb
unknown <malin383@CE105WD554110S.hillnet.ad.hsd.k12.or.us>
2015-05-29 15:30:22 -0700

    New Script, Follow Tutorial

--- Unity 2D Project/Assets/Scripts/Movement.cs
+++ Unity 2D Project/Assets/Scripts/Movement.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
+	Rigidbody2D rbody;
+	Animator anim;
+
 	// Use this for initialization
 	void Start () {
 	
+		rbody = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Vector2 movement_vector = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		if (movement_vector != Vector2.zero) {
+			anim.SetBool ("isWalking", true);
+			anim.SetFloat("input_x", movement_vector.x);
+			anim.SetFloat("input_x", movement_vector.y);
+		} else {
+			anim.SetBool ("isWalking", false);
+		}
+		rbody.MovePosition( rbody.position + movement_vector * Time.deltaTime);
 	}
 }
