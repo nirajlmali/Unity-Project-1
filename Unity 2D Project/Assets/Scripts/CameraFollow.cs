f958d692cf804967add8224d3366807fb394c0f6
unknown <malin383@CE105WD554110S.hillnet.ad.hsd.k12.or.us>
2015-06-04 15:28:07 -0700

    Camera Movement now

---
+++ Unity 2D Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target;
+	Camera mycam;
+
+	// Use this for initialization
+	void Start () {
+
+		mycam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		mycam.orthographicSize = (Screen.height / 100f) / 4f;
+		if (target) {
+
+			transform.position = Vector3.Lerp(transform.position, target.position)
+		}
+
+	}
+}
